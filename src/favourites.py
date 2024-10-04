7dc9e96f77bd4c7408d3892728a327edc05de7b9
klaas <klaas.hofman@gmail.com>
2024-10-04 17:14:54 +0200

    connect to speaker, list favourites and select when appropriate

---
+++ src/favourites.py
@@ -0,0 +1,80 @@
+from soco import SoCo
+from soco import discover
+from soco import music_library
+import curses
+
+
+# TODO: move this to a config file
+SONOS_SPEAKER_NAME = 'Rimshot HQ'
+
+def find_speaker(speaker_name):    
+    # sometimes the spaeker is not found on the first try
+    for i in range(0, 100):
+        zone_list = list(discover())
+        for zone in zone_list:
+            if (zone.player_name == speaker_name):
+                return zone
+    return None        
+
+def display_menu(stdscr, selected_row_idx, items):
+    stdscr.clear()
+    h, w = stdscr.getmaxyx()
+    
+    for idx, item in enumerate(items):
+        #x = w//2 - len(item)//2
+        #y = h//2 - len(items)//2 + idx
+        x = 0
+        y = idx
+        if idx == selected_row_idx:
+            stdscr.attron(curses.A_BOLD)
+            stdscr.addstr(y, x, '< ' + item + ' >')
+            stdscr.attroff(curses.A_BOLD)
+        else:
+            stdscr.addstr(y, x, item)
+    
+    stdscr.refresh()
+
+def main(stdscr):
+    speaker = find_speaker(SONOS_SPEAKER_NAME)
+    if speaker is None:
+        print(f"Speaker {SONOS_SPEAKER_NAME} not found")
+        exit(1)
+
+    # get the favouurite radio stations
+    search_results = speaker.music_library.get_sonos_favorites()
+    if search_results.number_returned == 0:
+        print("No favourites found")
+        exit(0)
+    
+    # obtain the list of radio stations
+    radio_stations = search_results._metadata['item_list']
+    for station in radio_stations:
+        print(f"{station.favorite_nr} - {station.title}") 
+    
+    items = [station.title for station in radio_stations]
+    selected_row_idx = 0
+
+    while True:
+        key = stdscr.getch()
+        
+        if key == curses.KEY_UP and selected_row_idx > 0:
+            selected_row_idx -= 1
+        elif key == curses.KEY_DOWN and selected_row_idx < len(items) - 1:
+            selected_row_idx += 1
+        # play the selected radio station
+        elif key == ord('\n'):
+            stdscr.addstr(0, 0, f"You selected {items[selected_row_idx]}")
+            stdscr.refresh()            
+            stdscr.getch()
+            uri = radio_stations[selected_row_idx].get_uri()
+            speaker.clear_queue()
+            speaker.add_uri_to_queue(uri=uri)
+            speaker.play_from_queue(0)
+            speaker.set_mute(False)
+            #speaker.music_library.play_uri(uri=uri, force_radio=True)
+            #speaker.play()
+            break
+        
+        display_menu(stdscr, selected_row_idx, items)
+
+curses.wrapper(main)
